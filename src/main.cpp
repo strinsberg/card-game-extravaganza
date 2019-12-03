fe386164bfa0d4bbf5ab5103615f445a32b9ec6f
deus3720 <deutekom@uleth.ca>
2019-12-03 09:04:23 -0700

    Abandoned idea to make an app that properly plays many rounds of rummy. Put extra game start logic in main to build the game according to player input.

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,65 @@
+#include <iostream>
 #include "OldMaidUI.h"
 #include "OldMaid.h"
 #include "OldMaidDeck.h"
+#include "Game.h"
+#include "Rummy.h"
+#include "RummyDeck.h"
+#include "RummyUI.h"
+
+const std::string players[] = {"Wendy", "John", "Barry", "Silvan"};
 
 int main() {
-    Deck* d = new OldMaidDeck();
-    d->create();
-    GameUI* ui = new OldMaidUI();
-    Game* g = new OldMaid(ui, d);
+    // Ask which game to play
+    int game;
+    
+    std::cout << "Welcome to the card Game extravaganza!!!\n\n"; 
+    std::cout << "What game would you like to play?\n";
+    std::cout << "1. Rummy\n2. Old Maid\n";
+    std::cout << "Choice: ";
 
-    g->addPlayer(new Player("John"));
-    g->addPlayer(new Player("Danielle"));
-    g->addPlayer(new Player("Tara"));
+    std::cin >> game;
 
+    // Build game objects
+    Game* g;
+    if (game == 1) {
+      RummyDeck* rd = new RummyDeck();
+      rd->create();
+      RummyUI* rui = new RummyUI();
+      g = new Rummy(rui, rd);
+
+    } else if (game == 2) {
+      OldMaidDeck* omd = new OldMaidDeck();
+      omd->create();
+      OldMaidUI* omui = new OldMaidUI();
+      g = new OldMaid(omui, omd);
+
+    } else {
+      std::cout << "Not a valid Choice!!\nBetter luck next time!\n";
+      return 0;
+    }
+
+    // Ask for players
+    std::string name;
+    std::cout << "What is your name (one word)? ";
+    std::cin >> name;
+
+    int playerNum = 1;
+    while (playerNum < 4 && playerNum > 0) {
+      std::cout << "\nHow many opponents (1-3)? ";
+      std::cin >> playerNum;
+    }
+
+    g->addPlayer(new Player(name));
+    for (int i = 0; i < playerNum; i++)
+      g->addPlayer(new Player(players[i]));
+
+    // Start game
     g->start();
 
+    std::cout << "\nThanks for playing!\n\n";
+
     delete g;
+
+    return 0;
 }
